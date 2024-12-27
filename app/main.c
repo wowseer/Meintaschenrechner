2d8b913e92d2964cde2bce8feb3cad2cc1449dc8
wowseer <test@test.de>
2024-12-27 20:06:26 +0100

    kurze menü überarbeitung

--- app/main.c
+++ app/main.c
@@ -4,7 +4,7 @@
 
 int main () {
 
-    int selection = menu();
+    int selection;
   do {
         selection = menu();
 
@@ -37,7 +37,7 @@ int menu(){
     int selection;
 
     printf("Hallo ich bin dein Persönlicher Taschenrechner\n");
-    printf("Bitte Gib an ob du Addieren(1), Subtrahieren(2), Multiplizieren(3) oder Dividieren(4) möchtest.\n");
+    printf("Bitte Gib an ob du Addieren(1), Subtrahieren(2), Multiplizieren(3) oder Dividieren(4) oder (0) bennden möchtest.\n");
     printf("\n\n");
     scanf("%d", &selection);
 
